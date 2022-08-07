3475fa85451376d18517a55418b2a2940aab8fd1
UzielCarranza <uzielcarranzac97@gmail.com>
2022-08-06 22:35:40 -0400

    fizzbuzz

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
-console.log("hello")
+console.log("Fizzbuzz");
+console.log("Given an integer n, return a string array answer (i -indexed) where  ");
+console.log("answer[i] == \"FizzBuzz\" if i is divisible by 3 and 5.\n" +
+    "answer[i] == \"Fizz\" if i is divisible by 3.\n" +
+    "answer[i] == \"Buzz\" if i is divisible by 5.\n" +
+    "answer[i] == i (as a string) if none of the above conditions are true");
